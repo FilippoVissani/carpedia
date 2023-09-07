3b50061af0ee233b12309bac5753cfe52a25749d
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-09-07 20:43:53 +0200

    chore: remove unused files

--- ontology_generator/lib/swrl_rules.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-require 'rdf'
-require_relative 'ontology_definitions'
-
-swrlRule = RDF::URI.new('http://example.org/swrlRule')
-
-graph << [swrlRule, RDF.type, RDF::OWL['Annotation']]
-graph << [swrlRule, RDF.value, "Car(?x) ∧ hasManufacturer(?x, ?y) → Manufacturer(?y)"]
