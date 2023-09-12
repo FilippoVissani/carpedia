23edeb10d866cbfd40c3ed443751f752723c6bef
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-09-12 16:15:35 +0200

    feat: add data properties

--- ontology_generator/lib/data_properties.rb
+++ ontology_generator/lib/data_properties.rb
@@ -6,3 +6,12 @@ require_relative 'graph_builder'
 require_relative 'carpedia_graph'
 include RDF
 include GraphBuilder
+
+CarpediaGraph
+  .with_data_property('hasKilometers', ['Car'], [XSD.integer])
+  .with_data_property('hasPrice', ['Car'], [XSD.integer])
+  .with_data_property('hasYearOfProduction', ['Car'], [XSD.date])
+  .with_data_property('hasPowerCV', ['Engine'], [XSD.integer])
+  .with_data_property('hasNumberOfDoors', ['Model'], [XSD.integer])
+  .with_data_property('hasNumberOfSeats', ['Model'], [XSD.integer])
+  .with_data_property('hasColor', ['Body'], [XSD.hexBinary])
