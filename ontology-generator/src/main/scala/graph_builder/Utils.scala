d05b0fcc66c4a34211e6801eafc6b4c0765ef161
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-09-13 22:31:45 +0200

    chore: format code

--- ontology-generator/src/main/scala/graph_builder/Utils.scala
+++ ontology-generator/src/main/scala/graph_builder/Utils.scala
@@ -6,7 +6,9 @@ object Utils:
   case class Hierarchy(subclass: String, superclass: String)
   case class ObjectProperty(identifier: String, domains: List[String], ranges: List[String])
   case class DataProperty(identifier: String, domains: List[String], dataTypes: List[Resource])
-  case class Individual(identifier: String,
-                        classIdentifier: String,
-                        objProperties: Map[String, String],
-                        dataProperties: Map[String, String])
+  case class Individual(
+      identifier: String,
+      classIdentifier: String,
+      objProperties: Map[String, String],
+      dataProperties: Map[String, String]
+  )
