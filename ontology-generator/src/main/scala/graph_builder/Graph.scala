d41bffd1a54f2c7cfa3312238fe1ca6ea3e27362
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-09-13 22:04:00 +0200

    refactor: add Scala 3 syntax

--- ontology-generator/src/main/scala/graph_builder/Graph.scala
+++ ontology-generator/src/main/scala/graph_builder/Graph.scala
@@ -2,94 +2,83 @@ package graph_builder
 
 import org.apache.jena.rdf.model.*
 import org.apache.jena.vocabulary.{OWL, RDF, RDFS}
-
 import java.io.*
 
-object GraphBuilder {
-  class Graph(baseUri: String) {
+trait Graph:
+  def withClass(classIdentifier: String): Graph
+  def withSubclass(subclassIdentifier: String, superclassIdentifier: String): Graph
+  def withObjectProperty(propertyIdentifier: String, domains: List[String], ranges: List[String]): Graph
+
+  def withDataProperty(propertyIdentifier: String, domains: List[String], datatypes: List[Resource]): Graph
+
+  def withIndividual(individualIdentifier: String,
+                     classIdentifier: String,
+                     objProperties: Map[String, String],
+                     dataProperties: Map[String, String]
+                    ): Graph
+  def dump(format: String, filename: String): Unit
+
+object Graph:
+  def apply(baseUri: String): Graph = GraphImpl(baseUri)
+  private class GraphImpl(baseUri: String) extends Graph:
     private val model: Model = ModelFactory.createDefaultModel()
     private val baseResource: Resource = model.createResource(baseUri)
 
-    // Define a class in the ontology
-    def withClass(classIdentifier: String): this.type = {
+    override def withClass(classIdentifier: String): Graph =
       val classResource: Resource = model.createResource(s"$baseUri/$classIdentifier")
       classResource.addProperty(RDF.`type`, OWL.Class)
       this
-    }
 
-    // Define a subclass in the ontology
-    def withSubclass(subclassIdentifier: String, superclassIdentifier: String): this.type = {
+    override def withSubclass(subclassIdentifier: String, superclassIdentifier: String): Graph =
       val subclassResource: Resource = model.createResource(s"$baseUri/$subclassIdentifier")
       val superclassResource: Resource = model.createResource(s"$baseUri/$superclassIdentifier")
       subclassResource.addProperty(RDF.`type`, OWL.Class)
       subclassResource.addProperty(RDFS.subClassOf, superclassResource)
       this
-    }
 
-    // Define an object property in the ontology
-    def withObjectProperty(propertyIdentifier: String, domains: List[String], ranges: List[String]): this.type = {
+    override def withObjectProperty(propertyIdentifier: String, domains: List[String], ranges: List[String]): Graph =
       val propertyResource: Resource = model.createResource(s"$baseUri/$propertyIdentifier")
       propertyResource.addProperty(RDF.`type`, OWL.ObjectProperty)
-
       domains.foreach { domain =>
         val domainResource: Resource = model.createResource(s"$baseUri/$domain")
         propertyResource.addProperty(RDFS.domain, domainResource)
       }
-
       ranges.foreach { range =>
         val rangeResource: Resource = model.createResource(s"$baseUri/$range")
         propertyResource.addProperty(RDFS.range, rangeResource)
       }
-
       this
-    }
 
-    // Define a data property in the ontology
-    def withDataProperty(propertyIdentifier: String, domains: List[String], datatypes: List[Resource]): this.type = {
+    override def withDataProperty(propertyIdentifier: String, domains: List[String], datatypes: List[Resource]): Graph =
       val propertyResource: Resource = model.createResource(s"$baseUri/$propertyIdentifier")
       propertyResource.addProperty(RDF.`type`, OWL.DatatypeProperty)
-
       domains.foreach { domain =>
         val domainResource: Resource = model.createResource(s"$baseUri/$domain")
         propertyResource.addProperty(RDFS.domain, domainResource)
       }
-
       datatypes.foreach { datatype =>
         propertyResource.addProperty(RDFS.range, datatype)
       }
-
       this
-    }
 
-    // Define an individual in the ontology
-    def withIndividual(
-                        individualIdentifier: String,
-                        classIdentifier: String,
-                        objProperties: Map[String, String],
-                        dataProperties: Map[String, String]
-                      ): this.type = {
+    override def withIndividual(individualIdentifier: String,
+                       classIdentifier: String,
+                       objProperties: Map[String, String],
+                       dataProperties: Map[String, String]): Graph =
       val individualResource: Resource = model.createResource(s"$baseUri/$individualIdentifier")
       val classResource: Resource = model.createResource(s"$baseUri/$classIdentifier")
       individualResource.addProperty(RDF.`type`, classResource)
-
       objProperties.foreach { case (property, value) =>
         val propertyResource: Property = model.createProperty(s"$baseUri/$property")
         val valueResource: Resource = model.createResource(s"$baseUri/$value")
         individualResource.addProperty(propertyResource, valueResource)
       }
-
       dataProperties.foreach { case (property, value) =>
         val propertyResource: Property = model.createProperty(s"$baseUri/$property")
         individualResource.addProperty(propertyResource, value)
       }
-
       this
-    }
 
-    // Dump the ontology graph to a specified format and file
-    def dump(format: String, filename: String = "ontology"): Unit = {
+    override def dump(format: String, filename: String): Unit =
       val outputStream: OutputStream = new FileOutputStream(s"$filename")
       model.write(outputStream, format)
-    }
-  }
-}
--- ontology-generator/src/main/scala/main.scala
+++ ontology-generator/src/main/scala/main.scala
@@ -1,13 +1,13 @@
-import graph_builder.GraphBuilder.Graph
+import graph_builder.Graph
 
 @main
 def main(): Unit = {
-  val graph = new Graph("http://example.org/ontology")
+  val graph = Graph("http://example.org/ontology")
   graph.withIndividual(
     "JohnDoe",
     "Person",
     Map("hasFriend" -> "Alice"),
     Map("hasAge" -> "30")
   )
-  graph.dump("RDF/XML-ABBREV")
+  graph.dump("RDF/XML-ABBREV", "ontology")
 }
