8e237ed2d454fdbcd8ee8bcffb69c73f41a23f73
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-09-15 10:05:48 +0200

    feat!: remove ontology generator

--- ontology_generator/lib/carpedia_graph.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-require 'rdf'
-require 'rdf/rdfxml'
-require 'rdf/turtle'
-require_relative 'graph_builder'
-include RDF
-include GraphBuilder
-
-CarpediaGraph = GraphBuilder::Graph.create('http://www.semanticweb.org/filippo/ontologies/2023/8/carpedia')
