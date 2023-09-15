d5d5ff5d9f96aacc9b0755858b2408a11f1a03c3
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-09-15 10:05:48 +0200

    feat!: remove ontology generator

--- ontology_generator/lib/data_properties.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-require 'rdf'
-require 'rdf/rdfxml'
-require 'rdf/turtle'
-require_relative 'graph_builder'
-require_relative 'carpedia_graph'
-include RDF
-include GraphBuilder
